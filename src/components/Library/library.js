8894af6ce6fad646c0db8097be3f9c6de54474ab
macbookair <macbookair@macbookairs-MacBook-Air.local>
2018-09-30 11:25:52 -0700

    built library course component with grid and library grid

--- src/components/Library/library.js
+++ src/components/Library/library.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 
+import LibraryCourse from './libraryCourse';
+
 class Library extends Component {
-    render (){
+    render() {
         return (
             <div className="library">
-
-                 <h1 className="library__title">Course Library</h1>
-
+                <h1 className="library__title">Course Library</h1>
+                <LibraryCourse/>
+                <LibraryCourse/>
+                <LibraryCourse/>
             </div>
-         
         )
     }
 }
